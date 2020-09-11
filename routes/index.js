e7f1beb311181d767515cb18439b6956fc40b5bc
Murilo Roque <muriloengqui@gmail.com>
2020-09-11 05:26:42 -0300

    fix linter errors

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: "Mini Messageboard", messages: messages });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Messageboard', messages });
 });
 
 module.exports = router;
